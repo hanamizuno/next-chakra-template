5d3d201bbbf11c6e1dd229bc1e7310a3a207e1cf
MIZUNO Kosuke <kosukenozumi@gmail.com>
2021-05-18 01:48:07 +0900

    Update index to use Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-export default function Home() {
-  return (
-    <div>
-      Index
-    </div>
-  )
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Home(): React.ReactElement {
+  return <Layout>Index Page</Layout>
 }
