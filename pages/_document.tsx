d01a6ae692c3f899b3d1aa7dff5d216ae22a2e8c
MIZUNO Kosuke <kosukenozumi@gmail.com>
2021-10-12 07:00:01 +0900

    Add eslint disable to ignore ChakraUI type error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react"
 import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
